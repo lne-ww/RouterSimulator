d8780b17111ee4f64eb2806cd6f126201759195a
えww <53077766+lne-ww@users.noreply.github.com>
2023-11-27 22:51:46 +0800

    1

---
+++ Main.cpp
@@ -0,0 +1,5 @@
+//Network n;
+//Device a,b;
+//n.addD(a) ... b;
+//a.send(p)
+//b.todo();
--- Network.h
+++ Network.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "Router.h"
+#include "Computer.h"
 class Network
 {
 public:
 	Network(Router* r);
+	void addComputer(Computer* c);
 
 private:
 	Router* router;
