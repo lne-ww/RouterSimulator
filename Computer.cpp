e9f1108832e4553018f222f285d3a274c8639c84
Everwh <132215078+Everwh@users.noreply.github.com>
2023-11-28 20:03:57 +0800

    update

--- Computer.cpp
+++ Computer.cpp
@@ -1,15 +1,15 @@
 #include "Computer.h"
 #include <iostream>
 
-Computer::Computer(std::string _address, std::string _name, std::string _itf, Router* _routerPtr) :
-	Device(_address, _name), itf(_itf), routerPtr(_routerPtr)
+Computer::Computer(std::string _address, std::string _name, const Itf& _itf)
+	:Device(_address, _name), itf(_itf)
 {
 }
 
 bool Computer::send(Packet p) const
 {
 	std::cout << "Computer " << getName() << " sent a packet." << std::endl;
-	return routerPtr->send(p);
+	return itf.devicePtr->send(p);
 }
 
 bool Computer::receive(Packet p) const
@@ -17,4 +17,9 @@ bool Computer::receive(Packet p) const
 	std::cout << "Computer " << getName() << " receive a packet from router." << std::endl;
 	std::cout << p;
 	return true;
+}
+
+Itf Computer::getItf() const
+{
+	return itf;
 }
